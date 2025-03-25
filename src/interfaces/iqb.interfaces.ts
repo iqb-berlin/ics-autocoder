0e5e54d88fe1da823fb2fd5a4e0c78763998415a
paflov <philipp.franck@hu-berlin.de>
2025-03-25 15:34:07 +0100

    Frontend: Attempt #1 to allow arrays in OptionSet

--- src/interfaces/iqb.interfaces.ts
+++ src/interfaces/iqb.interfaces.ts
@@ -9,13 +9,17 @@ export type AutoCodingInstructions = {
   [Member in keyof CodingScheme]: CodingScheme[Member];
 };
 
-export function isA<K>(collection: string[] | readonly string[], str: unknown): str is K {
-  return (typeof str === "string") && (collection as readonly string[]).includes(str);
-}
+export const isA = <K>(collection: string[] | readonly string[], str: unknown): str is K =>
+  (typeof str === "string") &&
+  (collection as readonly string[]).includes(str)
 
-export function isArrayOf<T>(thing: unknown, typeGuard: ((t: unknown) => t is T)): thing is T[] {
-  return Array.isArray(thing) && thing.every(typeGuard);
-}
+export const isArrayOf = <T>(thing: unknown, typeGuard: ((t: unknown) => t is T)): thing is T[] =>
+  Array.isArray(thing) && thing.every(typeGuard);
+
+export const isMapOf = <T>(thing: unknown, typeGuard: ((t: unknown) => t is T)): thing is {  [key: string]: T } =>
+  (typeof thing === 'object') &&
+  (thing != null) &&
+  isArrayOf(Object.values(thing), typeGuard)
 
 export const contains = <Key extends PropertyKey, Z>(thing: unknown, fieldName: Key, example: Z):
   thing is Record<Key, Z> =>
@@ -30,20 +34,36 @@ export const isCarrier = <Key extends string, Z extends string>(thing: unknown, 
   (typeof thing[fieldName] === "string") && (collection as readonly string[]).includes(thing[fieldName as string]);
 
 export const isResponseValueType =
-  (obj: unknown): obj is ResponseValueType =>
-    (Array.isArray(obj) && obj.every(isResponseValueType)) ||
-    (['string', 'number', 'boolean'].includes(typeof obj)) ||
-    (obj == null);
+  (thing: unknown): thing is ResponseValueType =>
+    (Array.isArray(thing) && thing.every(isResponseValueType)) ||
+    (['string', 'number', 'boolean'].includes(typeof thing)) ||
+    (thing == null);
 
 export const isResponse =
-  (obj: unknown): obj is Response =>
-    (typeof obj === 'object') && (obj != null) &&
-    ('id' in obj) && (typeof obj.id === "string") &&
-    ('status' in obj) && isA<ResponseStatusType>(ResponseStatusList, obj.status) &&
-    ('value' in obj) && isResponseValueType(obj.value) &&
-    (!('subform' in obj) || (typeof obj.subform === 'string')) &&
-    (!('code' in obj) || (typeof obj.code === 'number')) &&
-    (!('score' in obj) || (typeof obj.score === 'number'));
+  (thing: unknown): thing is Response =>
+    (typeof thing === 'object') && (thing != null) &&
+    ('id' in thing) && (typeof thing.id === "string") &&
+    ('status' in thing) && isA<ResponseStatusType>(ResponseStatusList, thing.status) &&
+    ('value' in thing) && isResponseValueType(thing.value) &&
+    (!('subform' in thing) || (typeof thing.subform === 'string')) &&
+    (!('code' in thing) || (typeof thing.code === 'number')) &&
+    (!('score' in thing) || (typeof thing.score === 'number'));
 
 export const isResponseList = (thing: unknown): thing is Response[] =>
   isArrayOf<Response>(thing, isResponse);
+
+export const debugTypeguard = <T>(input: unknown, typeguard: (thing: unknown) =>  thing is T): input is T => {
+  console.log('debug-typeguard:', typeguard.name);
+  console.log('test-object:', input);
+  const conditions = typeguard.toString().split('=>', 2)[1].split('&&');
+  const firstFailingCondition = conditions
+    .filter(condition => !!condition)
+    .find(condition => {
+      console.log('condition:', condition);
+      const result = eval(`(function test(thing) { return ${condition}; })`)(input);
+      console.log(result);
+      return !result;
+    });
+  console.log('final result:', !firstFailingCondition);
+  return !firstFailingCondition;
+}
