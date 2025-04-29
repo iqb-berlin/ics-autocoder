9f31e5721ff64bf98970a02872c57453f735fac9
paflov <philipp.franck@hu-berlin.de>
2025-04-29 12:56:27 +0200

    stop tracking dist dir

--- dist/src/services/id.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class IdService {
-    static create(length?: number, alphabet?: string): string;
-}
--- dist/src/services/worker/worker.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { TasksService } from '../tasks/tasks.service';
-import { DataService } from '../data/data.service';
-export declare class WorkerService {
-    private readonly ts;
-    private readonly ds;
-    private runner;
-    constructor(ts: TasksService, ds: DataService);
-    private code;
-}
