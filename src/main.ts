f3d674b597d76a03e427ecd6782f6b135898a852
paflov <philipp.franck@hu-berlin.de>
2025-06-13 14:21:26 +0200

    Make CORS configurable

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule.init());
-  app.enableCors();
+  const allowedOrigins = process.env.CORS_ALLOW_ORIGINS || 'http://localhost:4200';
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
